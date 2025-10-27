3c16141a63de7f1f6005379bce27eaa25c030f62
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-10-27 21:35:21 +0500

    learning pthon

--- hello.py
+++ hello.py
@@ -1,14 +1,20 @@
-print('Welcome to Programming Fundamentals Lab 01!')
+# print('Welcome to Programming Fundamentals Lab 01!')
 
-# setting the values
-age = ('18')
-name = ('Shaban')
-height = ("5'7")
-is_student = True
+# # setting the values
+# age = ('18')
+# name = ('Shaban')
+# height = ("5'7")
+# is_student = True
 
-# printing the values
-print(age)
-print(name)
-print(height)
-print(is_student)
+# # printing the values
+# print(age)
+# print(name)
+# print(height)
+# print(is_student)
 
+# task 3
+num_1 = int(67)
+num_2 = int(33)
+
+sum = num_1 + num_2
+print(sum)
