63d648d76e8c6b5c9074aa0574f16d853e1c54b8
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-10-27 21:32:32 +0500

    Initial commit

---
+++ hello.py
@@ -0,0 +1,14 @@
+print('Welcome to Programming Fundamentals Lab 01!')
+
+# setting the values
+age = ('18')
+name = ('Shaban')
+height = ("5'7")
+is_student = True
+
+# printing the values
+print(age)
+print(name)
+print(height)
+print(is_student)
+
