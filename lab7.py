6836eef029dea1ea9994262788a3ca080edac14c
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-23 15:08:40 +0500

    trying to make the code shorter

--- lab7.py
+++ lab7.py
@@ -1,7 +1,17 @@
+# Game Title
+print('GUESS THE COMPUTERS MOVE')
+
+# Taking User Input
 userMove = input('choose your move:')
-from random import random
-computerMove = 'heads' if random() < 0.5 else 'tails'
-result = 'you WIN' if userMove == computerMove else 'you LOSE'
-print('Computer Move: ', computerMove)
 print('You Chose', userMove.upper())
+
+
+# importing method
+from random import random
+# Random Number
+randomNumber = random()
+
+computerMove = 'heads' if randomNumber < 0.5 else 'tails'
+
+result = 'you WIN' if userMove == computerMove else 'you LOSE'
 print('Result:', result)
