3fb285b2e1462bf7b74a0bb37493eb73f16eae89
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-11-23 15:07:44 +0500

    tighter version

--- lab7.py
+++ lab7.py
@@ -1,5 +1,7 @@
-from random import random
 userMove = input('choose your move:')
+from random import random
 computerMove = 'heads' if random() < 0.5 else 'tails'
 result = 'you WIN' if userMove == computerMove else 'you LOSE'
-print('Computer Chose: ', computerMove, ', You Chose', userMove, ', Result:', result)
+print('Computer Move: ', computerMove)
+print('You Chose', userMove.upper())
+print('Result:', result)
